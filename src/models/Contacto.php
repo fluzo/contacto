10da07da2bf526ccb46dc20d7193152609621a82
Javi <fluzo.info@gmail.com>
2014-05-21 21:08:52 +0200

    Añadida validación expresión regular al nombre

--- src/models/Contacto.php
+++ src/models/Contacto.php
@@ -1,16 +1,19 @@
-<?php 
+<?php
 
 class Contacto
 {
+
     private static $reglas = array(
-        'nombre' => array('required', 'min:2', 'max:50'), 
-            //'regex:/^([a-zA-ZñÑáéíóúÁÉÍÓÚ_-])+((\s*)+([a-zA-ZñÑáéíóúÁÉÍÓÚ_-]*)*)+$/'),
+        'nombre' => array('required', 'min:2', 'max:50',
+            'regex:/^([a-zA-ZñÑáéíóúÁÉÍÓÚ_-])+((\s*)+([a-zA-ZñÑáéíóúÁÉÍÓÚ_-]*)*)+$/'),
         'email' => 'required | email | min:5 | max:150',
         'mensaje' => 'required | max:5000'
     );
+    private static $mensajes = array('regex' => 'El formato de el :attribute no es correcto, solo se permiten letras, números, espacios en blanco
+    y guiones medios o bajos.');
     public static function valida()
-    {        
-        $validador = Validator::make(Input::all(), self::$reglas); // Validamos el formulario       
+    {
+        $validador = Validator::make(Input::all(), self::$reglas, self::$mensajes); // Validamos el formulario       
         return $validador;
     }
 
