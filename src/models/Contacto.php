0adcecad1b009829ad94d40111bd11d42001006c
Javi <fluzo.info@gmail.com>
2014-05-22 10:56:14 +0200

    Añadida validación expresión regular al nombre

--- src/models/Contacto.php
+++ src/models/Contacto.php
@@ -5,7 +5,7 @@ class Contacto
 
     private static $reglas = array(
         'nombre' => array('required', 'min:2', 'max:50',
-            'regex:/^([a-zA-ZñÑáéíóúÁÉÍÓÚ_-])+((\s*)+([a-zA-ZñÑáéíóúÁÉÍÓÚ_-]*)*)+$/'),
+            'regex:/^([0-9a-zA-ZñÑáéíóúÁÉÍÓÚ_-])+((\s*)+([0-9a-zA-ZñÑáéíóúÁÉÍÓÚ_-]*)*)+$/'),
         'email' => 'required | email | min:5 | max:150',
         'mensaje' => 'required | max:5000'
     );
